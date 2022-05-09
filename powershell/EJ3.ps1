1534ed0c4404a987ad81975cec0010f2fd66779f
Donatella Fragassi <dfragassi@circostudio.com>
2022-05-09 20:11:41 -0300

    validaciones

--- powershell/EJ3.ps1
+++ powershell/EJ3.ps1
@@ -12,6 +12,39 @@ Param(
     [string]$s
 
 )
-# foreach($linea in $a){
-#     write-host $linea
-# }
+
+$flagPub=0;
+$flagComp=0;
+
+foreach($linea in $a){
+   if($linea -eq "compilar"){
+       $flagComp=1;
+   }
+   if($linea -eq "publicar"){
+       $flagPub=1;
+   }
+}
+
+if($flagPub -eq 1){
+    if(!($flagComp -eq 1)){
+        Write-Error ("No se puede publicar sin compilar. Intentelo nuevamente.");
+    }
+}
+
+
+if(!$s -and $flagPub -eq 1){
+    write-Error("No se puede publicar sin una direccion. Intentelo nuevamente.")
+} else {
+    if($flagPub -eq 1 -and $s){
+        if(!(Test-Path  $s)){
+            #creo el path si no existe    
+            New-Item -Path $s -ItemType directory | Out-Null
+        }
+    }
+}
+
+#como obtener el path absoluto:
+#$pathMonitorear = (Resolve-Path -Path "$c").Path;
+
+
+
